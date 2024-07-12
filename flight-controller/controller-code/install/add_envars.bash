445b7e46a56ada180f9c05129bd230d902eef397
Bobobo <me@you.com>
2024-07-12 14:36:29 -0500

    Various Install Changes

--- flight-controller/controller-code/install/add_envars.bash
+++ flight-controller/controller-code/install/add_envars.bash
@@ -4,13 +4,16 @@
 
 # simplifies coordination between udp_capture and
 # the controller program
-
-out_path="$HOME/detector-config/umn_detector_envars.txt"
-cp "envars.bash" $out_path
+cuser=${SUDO_USER:-$(whoami)}
+out_path="/home/${cuser}/detector-config/umn_detector_envars.txt"
+mkdir -p "$(dirname "$out_path")"
+cp "/home/${cuser}/umn-detector-code/flight-controller/controller-code/install/envars.bash" $out_path
 source_str="source $out_path"
 
-if [[ !( $(cat "$HOME/.bashrc") =~ $source_str ) ]]; then
-    echo $source_str >> "$HOME/.bashrc"
+if [[ !( $(cat "/home/${cuser}/.bashrc") =~ $source_str ) ]]; then
+    echo $source_str >> "/home/${cuser}/.bashrc"
 else
     echo "updated port"
 fi
+
+source /home/${cuser}/.bashrc
