922b7acc1939bd9459f2ba8f2ddec548c689d186
William Setterberg <sette095@umn.edu>
2024-07-21 14:33:11 -0500

    fix envars install to use relative path

--- flight-controller/controller-code/install/add_envars.bash
+++ flight-controller/controller-code/install/add_envars.bash
@@ -4,16 +4,29 @@
 
 # simplifies coordination between udp_capture and
 # the controller program
+
+if [[ $# != 1 ]]; then
+    echo "Need to supply folder with envars txt file in it as sole argument to $0."
+    exit 1
+fi
+
 cuser=${SUDO_USER:-$(whoami)}
 out_path="/home/${cuser}/detector-config/umn_detector_envars.txt"
 mkdir -p "$(dirname "$out_path")"
-cp "/home/${cuser}/umn-detector-code/flight-controller/controller-code/install/envars.bash" $out_path
-source_str="source $out_path"
 
-if [[ !( $(cat "/home/${cuser}/.bashrc") =~ $source_str ) ]]; then
-    echo $source_str >> "/home/${cuser}/.bashrc"
+
+base_path=$1
+cp "$base_path/envars.bash" $out_path || (echo "Failed to copy file envars file" && exit -1)
+
+
+source_str="source $out_path"
+brc_path="/home/${cuser}/.bashrc"
+
+if [[ !( $(cat $brc_path) =~ $source_str ) ]]; then
+    echo $source_str >> $brc_path
 else
     echo "updated port"
 fi
+
 
 source /home/${cuser}/.bashrc
