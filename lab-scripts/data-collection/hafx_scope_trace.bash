1b3371c98711c4f78937b1afe62cef574d34319b
WillHonnold <honno12@umn.edu>
2024-08-20 19:23:38 +0100

    alter scope trace script

--- lab-scripts/data-collection/hafx_scope_trace.bash
+++ lab-scripts/data-collection/hafx_scope_trace.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 [detector identifier]"
+    exit 1
+fi
+ 
+bash check_running.bash || exit 1
+
+det="$1"
 source udpcap_ports.bash
 echo "wake" > $det_udp_dev
-echo "debug hafx c1 fpga_oscilloscope_trace 1" > $det_udp_dev
+echo "debug hafx $det fpga_oscilloscope_trace 1" > $det_udp_dev
 sleep 3
 echo "sleep" > $det_udp_dev
 exit 0
