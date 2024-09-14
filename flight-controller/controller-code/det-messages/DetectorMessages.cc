5b62c2ca75c46d497b909acda9c4fb5f9494260c
bobillium <sette095@umn.edu>
2024-09-14 08:41:16 -0500

    add 'stripped' NRL data point

--- flight-controller/controller-code/det-messages/DetectorMessages.cc
+++ flight-controller/controller-code/det-messages/DetectorMessages.cc
@@ -9,4 +9,30 @@ X123Settings::X123Settings() :
     adc_rebin_edges_length{0}
 { }
 
+StrippedNrlDataPoint
+StrippedNrlDataPoint::from(const SipmUsb::NrlListDataPoint& orig) {
+    /*
+     * Strip down the NrlListDataPoint into a much smaller type.
+     * */
+
+    // Scale the LSB of the data by the DIVISOR
+    // Make them constexpr so they are genuine constants,
+    // but easier to change if we want to down the road
+    static constexpr uint64_t ORIG_DIV_NS = 25;
+    static constexpr uint64_t GOAL_DIV_NS = 200;
+    static_assert(GOAL_DIV_NS % ORIG_DIV_NS == 0,
+                  "Goal NRL time division must evenly divide by original time steps");
+    static constexpr uint64_t DIVISOR = GOAL_DIV_NS / ORIG_DIV_NS;
+
+    return {
+        // Convert to the time step specified above
+        .wall_clock = static_cast<uint32_t>((orig.wall_clock_time / DIVISOR)) & 0x1ffffff,
+        // Drop the bottom 12 bits of precision because EXACT
+        // doesn't really care about energy
+        .energy = static_cast<uint32_t>((orig.energy >> 12ULL)) & 0xf,
+        .was_pps = static_cast<uint8_t>(orig.was_pps),
+        .piled_up = static_cast<uint8_t>(orig.piled_up),
+        .out_of_range = static_cast<uint8_t>(orig.out_of_range)
+    };
+}
 };
--- flight-controller/controller-code/det-messages/DetectorMessages.hh
+++ flight-controller/controller-code/det-messages/DetectorMessages.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <netinet/in.h>
+#include <IoContainer.hh>
 
 #include <array>
 #include <ctime>
@@ -8,6 +9,7 @@
 #include <optional>
 #include <variant>
 #include <vector>
+#include <type_traits>
 
 namespace DetectorMessages {
     enum class HafxChannel : uint8_t {
@@ -182,4 +184,27 @@ namespace DetectorMessages {
         uint32_t time_anchor;
         bool missed_pps;
     };
+
+    // Strip down the NRL data to only the bits we care about
+    struct __attribute__((packed)) StrippedNrlDataPoint {
+        // Assumes that the 51-bit wall clock never exceeds
+        // ~5-10 seconds, which should be reasonable in this
+        // application.
+        // Assumes we scale down to 200ns precision (8 25ns divs),
+        // so with 25 bits, that gives a max time of 6.7s.
+        uint32_t wall_clock   : 25;
+        uint32_t energy       : 4;
+        uint32_t was_pps      : 1;
+        uint32_t piled_up     : 1;
+        uint32_t out_of_range : 1;
+
+        static StrippedNrlDataPoint
+        from(const SipmUsb::NrlListDataPoint& orig);
+    };
+    static_assert(
+        std::is_trivially_copyable_v<StrippedNrlDataPoint>,
+        "Stripped NRL struct must be a 'POD' type"
+    );
+    // Size of the _data_ is a uint32, even with the static method
+    static_assert(sizeof(StrippedNrlDataPoint) == sizeof(uint32_t));
 }
